407df2c1e1e583bd152ddd67279c5ef97f24ddc8
jaigs10 <jagasuar10@gmail.com>
2022-04-18 12:54:14 +0200

    jaime

---
+++ calculadora.js
@@ -0,0 +1,49 @@
+console.log('importado correctamente');
+
+ var Operaciones= ['+', '-', '*', '/'];
+
+         var num1 ;
+         var num2 ;
+         var resultado="";
+            
+        //     switch (elemento) {
+        //         case '+':
+        //            resultado= 'SUMA: ' + (num1+num2);   
+        //             break;
+        //         case '-':
+        //             resultado= 'RESTA: '+ (num1-num2); 
+        //             break;
+        //         case '*':
+        //             resultado= 'MULTPLICACIÓN: ' + (num1*num2); 
+        //              break;
+        //         case '/':
+        //             resultado= 'DIVISIÓN: ' + (num1/num2); 
+        //         break;
+            
+        //         default:
+        //             break;
+        //     }
+        //     console.log(`resultado ${resultado}`);
+        // }
+
+        function SUMA(){
+            console.log('Suma')
+            resultado= 'SUMA: ' + (num1+num2); 
+            
+        }
+        function RESTA(){
+            console.log('Resta');
+            resultado= 'RESTA: '+ (num1-num2); 
+         
+        }
+        function MULTIPLICACION() {
+            console.log('Multiplicación');
+            resultado= 'MULTPLICACIÓN: ' + (num1*num2); 
+           
+        }
+        function DIVISION(){
+            console.log('División')
+            resultado= 'DIVISIÓN: ' + (num1/num2); 
+            
+        }
+        
