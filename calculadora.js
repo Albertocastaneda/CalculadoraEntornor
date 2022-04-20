c105a6af046e087a3d7b74426c93d348bf6f045e
jaigs10 <jagasuar10@gmail.com>
2022-04-20 09:53:26 +0200

    Entrar

--- calculadora.js
+++ calculadora.js
@@ -28,15 +28,51 @@ console.log('importado correctamente');
             
         }
         
-        function igual(igual){
-            console.log(igual);
-            console.log(resultado);
+        function igual(){
+            
+            //en funcion de "operacion" y de num1 y num2, resultado va a ser una cosa u otra
+
+            // console.log('numero 1:', num1);
+            // console.log('numero 2:', num2);
+            // console.log('operacion:', operacion);
+
+            switch (operacion) {
+                case suma:
+                    resultado= num1 + num2;
+                    console.log(resultado);
+                    break;
+                case resta:
+                    resultado= num1 - num2;
+                    console.log(resultado);
+                    break;
+                case multiplicación:
+                    resultado= num1 * num2;
+                    console.log(resultado);
+                    break;
+                case division:
+                    resultado= num1/num2;
+                    console.log(resultado);
+                default:
+                    break;
+            }
+
+            num1 = null;
+            num2 = null;
+            operacion = null;
+
         }
 
         function conseguirNumero(num){
-            console.log(num)
+            // console.log(num)
+            if (operacion==null){
+                num1= num;
+            }else{
+                num2=num;
+            }
         }
-        function obtenerOperacion(operacion) {
-            console.log(operacion);
+        function obtenerOperacion(op) {
+            // console.log(operacion)
+            operacion = op;
+
         }
         
