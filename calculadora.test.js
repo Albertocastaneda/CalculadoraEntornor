70133a474e154c351de425a37e10b5c6c8f074fa
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-04-21 08:39:13 +0200

    Add files via upload

---
+++ calculadora.test.js
@@ -0,0 +1,17 @@
+var calculatora = require('./calculadora');
+
+test('addition 1 + 2 should be equals to 3', () => {
+    expect(calculatora.add(1, 2)).toBe(3);
+});
+
+test('substract 1 - 1 should be equals to 0', () => {
+    expect(calculatora.substract(1, 1)).toBe(0);
+});
+
+test('divide 2 by 2 should be equals to 1', () => {
+    expect(calculatora.divider(2, 2)).toBe(1);
+});
+
+test('multiply 3 by 2 should be equals to 6', () => {
+    expect(calculatora.multiplier(3, 2)).toBe(6);
+});
