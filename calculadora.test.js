d9f638c3e3167d203cddf1ac8035d9cd9b53ed71
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-11 10:42:42 +0200

    Update calculadora.test.js

--- calculadora.test.js
+++ calculadora.test.js
@@ -3,50 +3,48 @@ describe("comprobaciones iniciales", () => {
   test("todo ok", () => {
     expect(true).toBe(true);
   });
-})
-describe("calcular suma",()=>{
-     test("sumar numeros",()=>{
-      expect(calculatora.suma()).toBe("suma");
+});
+describe("calcular suma", () => {
+  test("sumar numeros", () => {
+    expect(calculatora.suma(1, 2)).toBe(3);
   });
-})
-describe("calcular resta",()=>{
-    test("restar numeros",()=>{
-      expect(calculatora.resta()).toBe("resta");
+});
+describe("calcular resta", () => {
+  test("restar numeros", () => {
+    expect(calculatora.resta(2,1)).toBe(1);
   });
-})
-describe("calcular multiplicación",()=>{
-    test("multiplicar numero",()=>{
-        expect(calculatora.multiplicación()).toBe("multiplicar");
-    });
-}) 
-describe("calcular división",()=>{
-    test("dividir numero",()=>{
-        expect(calculatora.division()).toBe("dividir");
-    });
-})
-
-describe("calcular igual",()=>{
-    test("igual",()=>{
-        expect(calculatora.igual()).toBe("igual");
-    });
-})
-
-describe("calcular potencia",()=>{
-    test("potencia",()=>{
-        expect(calculatora.potencia()).toBe("Potencia");
-    });
-})
-
-test("importa calc", () => {
-    expect(calculatora.prueba()).toBe("todo ok");
+});
+describe("calcular multiplicación", () => {
+  test("multiplicar numero", () => {
+    expect(calculatora.multiplicación(2,1)).toBe(2);
+  });
+});
+describe("calcular división", () => {
+  test("dividir numero", () => {
+    expect(calculatora.division(2,1)).toBe(2);
+  });
 });
 
-test("conseguirNumero",()=>{
-    expect(calculatora.conseguirNumero()).toBe("conseguir Número");
+describe("calcular potencia", () => {
+  test("potencia", () => {
+    expect(calculatora.potencia(3,2)).toBe(9);
+  });
 });
-test("obtenerOperación",()=>{
-    expect(calculatora.obtenerOperacion()).toBe("Obtener Oeración");
+
+test.skip("igual", () => {
+    expect(calculatora.igual()).toBe("igual");
 });
-test("GuardarResultado",()=>{
+
+test.skip("importa calc", () => {
+  expect(calculatora.prueba()).toBe("todo ok");
+});
+
+test.skip("conseguirNumero", () => {
+  expect(calculatora.conseguirNumero()).toBe("conseguir Número");
+});
+test.skip("obtenerOperación", () => {
+  expect(calculatora.obtenerOperacion()).toBe("Obtener Oeración");
+});
+test.skip("GuardarResultado", () => {
   expect(calculatora.GuardarResultado()).toBe("Guardar Resultado");
 });
